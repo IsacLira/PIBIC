759774f2583c9226a41a8e5ee16e69c39509c6b1
IsacLira <isac.lira@gmail.com>
2016-06-09 17:05:23 -0300

    Delete getCorrelationMatrix.m

--- getCorrelationMatrix.m
+++
@@ -1,8 +0,0 @@
-function [R]=getCorrelationMatrix(H)
-% Calculate spatial correlation matrix
-R=H*H';
-R=abs(diag(1./sqrt(diag(R)))*R*diag(1./sqrt(diag(R))));
-% Regularizes spatial correlation matrix
-R = R - min(0, min(eig(R)))*eye(size(R));
-
-end
